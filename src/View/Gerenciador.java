6fce1b7327573e35e19c6c54d162ec968bd8a841
LucasGuasselli <lucaszack265@gmail.com>
2017-03-18 18:24:27 -0300

    Adicionado pacote util com Digita, Opcao e Menu funcionando. Gerenciador nao terminado

--- src/View/Gerenciador.java
+++ src/View/Gerenciador.java
@@ -5,6 +5,13 @@
  */
 package View;
 
+import Model.Medicamentos;
+import Model.Pacientes;
+import java.util.ArrayList;
+import java.util.List;
+import util.Menu;
+import util.Opcao;
+
 /**
  *
  * @author lucas
@@ -12,5 +19,33 @@ package View;
 public class Gerenciador {
     public static void main(String[] args) {
         
+        
+       List<Pacientes> listaPaciente = new ArrayList<>();
+       List<Medicamentos> listaMedicamento = new ArrayList<>();
+
+       
+       Opcao cadPac = new Opcao("Cadastrar Paciente");
+       Opcao cadMedicam = new Opcao("Cadastrar Medicamento");
+       
+       Menu menu = new Menu();
+       menu.addOption(cadPac);
+       menu.addOption(cadMedicam);
+
+       
+       
+       
+       switch(menu.getOption()){
+           case 1:
+               
+               break;
+           case 2:
+               
+               break;
+           default:
+               System.out.println("Opcao Invalida!!!");
+               break;
+           
+           
+       }
     }//fecha main
 }//fecha classe
